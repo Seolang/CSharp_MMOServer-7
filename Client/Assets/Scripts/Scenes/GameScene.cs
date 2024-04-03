ed0ba221435303301939037f2ef6de960c274096
Seolang <dntmdqls0912@gmail.com>
2024-04-04 01:49:31 +0900

    refactoring old packet mechanism to google protobuf both client and server

--- Client/Assets/Scripts/Scenes/GameScene.cs
+++ Client/Assets/Scripts/Scenes/GameScene.cs
@@ -13,41 +13,29 @@ public class GameScene : BaseScene
 
         Managers.Map.LoadMap(1); // 1번 맵 로드
 
-        // 플레이어 생성 후 ObjectManager에 추가
-        GameObject player = Managers.Resource.Instantiate("Creature/Player");
-        player.name = "Player";
-        Managers.Object.Add(player);
+        //// 플레이어 생성 후 ObjectManager에 추가
+        //GameObject player = Managers.Resource.Instantiate("Creature/Player");
+        //player.name = "Player";
+        //Managers.Object.Add(player);
 
-        for (int i = 0; i < 5; i++)
-        {
-            // 몬스터 생성 후 ObjectManager에 추가
-            GameObject monster = Managers.Resource.Instantiate("Creature/Monster");
-            monster.name = $"Monster_{i + 1}";
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    // 몬스터 생성 후 ObjectManager에 추가
+        //    GameObject monster = Managers.Resource.Instantiate("Creature/Monster");
+        //    monster.name = $"Monster_{i + 1}";
 
-            // 랜덤 위치 지정
-            Vector3Int pos = new Vector3Int()
-            {
-                x = Random.Range(-20, 20),
-                y = Random.Range(-10, 10),
-            };
+        //    // 랜덤 위치 지정
+        //    Vector3Int pos = new Vector3Int()
+        //    {
+        //        x = Random.Range(-20, 20),
+        //        y = Random.Range(-10, 10),
+        //    };
 
-            MonsterController mc = monster.GetComponent<MonsterController>();
-            mc.CellPos = pos; // 몬스터의 초기 위치를 랜덤 위치로 이동
+        //    MonsterController mc = monster.GetComponent<MonsterController>();
+        //    mc.CellPos = pos; // 몬스터의 초기 위치를 랜덤 위치로 이동
 
-            Managers.Object.Add(monster);
-        }
-
-        //Managers.UI.ShowSceneUI<UI_Inven>();
-        //Dictionary<int, Data.Stat> dict = Managers.Data.StatDict;
-        //gameObject.GetOrAddComponent<CursorController>();
-
-        //GameObject player = Managers.Game.Spawn(Define.WorldObject.Player, "UnityChan");
-        //Camera.main.gameObject.GetOrAddComponent<CameraController>().SetPlayer(player);
-
-        ////Managers.Game.Spawn(Define.WorldObject.Monster, "Knight");
-        //GameObject go = new GameObject { name = "SpawningPool" };
-        //SpawningPool pool = go.GetOrAddComponent<SpawningPool>();
-        //pool.SetKeepMonsterCount(2);
+        //    Managers.Object.Add(monster);
+        //}
     }
 
     public override void Clear()
---
+++ Common/protoc-3.12.3-win64/bin/GenProto.bat
@@ -0,0 +1,9 @@
+protoc.exe -I=./ --csharp_out=./ ./Protocol.proto 
+rem 에러가 나면 정지 및 메세지 출력
+IF ERRORLEVEL 1 PAUSE 
+
+START ../../../Server/PacketGenerator/bin/PacketGenerator.exe ./Protocol.proto
+XCOPY /Y Protocol.cs "../../../Client/Assets/Scripts/Packet"
+XCOPY /Y Protocol.cs "../../../Server/Server/Packet"
+XCOPY /Y ClientPacketManager.cs "../../../Client/Assets/Scripts/Packet"
+XCOPY /Y ServerPacketManager.cs "../../../Server/Server/Packet"
