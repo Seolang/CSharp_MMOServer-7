3ef8fec087ac993c56e554d7b5ad10942ffe1865
Seolang <dntmdqls0912@gmail.com>
2024-06-08 03:27:33 +0900

    Feat: 평타, 화살 스킬 데이터 정의 및 데이터 로딩 로직 추가

--- Server/Server/Data/Data.Contents.cs
+++ Server/Server/Data/Data.Contents.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Google.Protobuf.Protocol;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -24,6 +25,41 @@ namespace Server.Data
             Dictionary<int, Stat> dict = new Dictionary<int, Stat>();
             foreach (Stat stat in stats)
                 dict.Add(stat.level, stat);
+            return dict;
+        }
+    }
+    #endregion
+
+    #region Skill
+    [Serializable]
+    public class Skill
+    {
+        public int id;
+        public string name;
+        public float cooldown;
+        public int damage;
+        public SkillType skillType;
+        public ProjectileInfo projectile;
+    }
+
+    public class ProjectileInfo
+    {
+        public string name;
+        public float speed;
+        public int range;
+        public string prefab;
+    }
+
+    [Serializable]
+    public class SkillData : ILoader<int, Skill>
+    {
+        public List<Skill> skills = new List<Skill>();
+
+        public Dictionary<int, Skill> MakeDict()
+        {
+            Dictionary<int, Skill> dict = new Dictionary<int, Skill>();
+            foreach (Skill skill in skills)
+                dict.Add(skill.id, skill);
             return dict;
         }
     }
--- Server/Server/Data/DataManager.cs
+++ Server/Server/Data/DataManager.cs
@@ -13,10 +13,12 @@ namespace Server.Data
     public class DataManager
     {
         public static Dictionary<int, Data.Stat> StatDict { get; private set; } = new Dictionary<int, Data.Stat>();
+        public static Dictionary<int, Data.Skill> SkillDict { get; private set; } = new Dictionary<int, Data.Skill>();
 
         public static void LoadData()
         {
             StatDict = LoadJson<Data.StatData, int, Data.Stat>("StatData").MakeDict();
+            SkillDict = LoadJson<Data.SkillData, int, Data.Skill>("SkillData").MakeDict();
         }
 
         static Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
